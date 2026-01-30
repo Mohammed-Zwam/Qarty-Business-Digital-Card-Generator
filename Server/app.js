1a5248844c1a77ff6fc983ba906efd51968fc2fd
Mohammed Ashraf <152704078+Mohammed-Zwam@users.noreply.github.com>
2026-01-30 20:49:10 +0200

    Serve Swagger UI via app.use('/')
    Replace app.get('/') with app.use('/', swaggerUi.serve, swaggerUi.setup(...))

--- Server/app.js
+++ Server/app.js
@@ -26,11 +26,11 @@ app.use(cookieParser());
 
 
 // ROUTES
-app.get('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(require('./middlewares/mongoDBConnectionMW')); // Check DB Connection (FIX: MongoDB Sleep)
 app.use("/auth", authRouter);
 app.use("/api/digital-card", digitalCardRouter);
-
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} ...`);
